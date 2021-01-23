442abcad6c4687358b4b5faf4b1f513769202e8a
Harshit Thukral <h.thakral369@gmail.com>
2021-01-23 21:46:50 +0530

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,47 @@
 <script lang="ts">
-	export let name: string;
+  import { here } from "await-here";
+  import type { Player } from "./entities/player";
+  import db from "./services/db";
+
+  let players: Player[] = [];
+
+  async function searchPlayer(e) {
+    const [err, records] = await here(
+      db.player.getAll({ search: e.target.value })
+    );
+    if (!err) {
+      players = records;
+    }
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello!</h1>
+  <input on:keyup={searchPlayer} />
+
+  {#each players as player}
+    <p>{player.name}</p>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/services/db.ts
+++ src/services/db.ts
@@ -1,1 +1,18 @@
-import {} from 'neo4j-driver';
+import { driver, auth, Session, session } from "neo4j-driver";
+
+import playerService from './players'
+
+export const driverInstance = driver(
+  // @ts-ignore
+  process.env.DB_URL,
+  // @ts-ignore
+  auth.basic(process.env.DB_USER, process.env.DB_PASSWORD)
+);
+
+function DB(session: Session) {
+  return {
+    player: playerService(session)
+  };
+}
+
+export default DB(driverInstance.session({ defaultAccessMode: session.READ }));
